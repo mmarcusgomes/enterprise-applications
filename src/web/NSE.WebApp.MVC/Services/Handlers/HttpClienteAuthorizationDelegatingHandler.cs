4eab093ca281e88132695dc5d08ed393d7a97932
Marcus Gomes <mmarcusgomes@hotmail.com>
2022-01-04 21:26:36 -0300

    Criado handler para interceptar request para outras APIs

---
+++ src/web/NSE.WebApp.MVC/Services/Handlers/HttpClienteAuthorizationDelegatingHandler.cs
@@ -0,0 +1,32 @@
+﻿using NSE.WebApp.MVC.Extensions;
+
+namespace NSE.WebApp.MVC.Services.Handlers
+{
+    public class HttpClienteAuthorizationDelegatingHandler:DelegatingHandler
+    {
+        private readonly IUser _user;
+
+        public HttpClienteAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHeader = _user.ObterHttpContext().Request.Headers["Authorization"];
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                request.Headers.Add("Authorization", new List<string>() { authorizationHeader });
+            }
+            var token = _user.ObterUserToken();
+
+            if (token != null)
+            {
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
